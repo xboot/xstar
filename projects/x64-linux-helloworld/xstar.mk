a5acd7f85ec7ddaee27e6f04f4ae2720cdfd05de
Jiang Jianjun <8192542@qq.com>
2026-07-15 00:45:04 +0800

    [x64-linux-helloworld]add generating sdk support

--- projects/x64-linux-helloworld/xstar.mk
+++ projects/x64-linux-helloworld/xstar.mk
@@ -20,3 +20,19 @@ ODFLAGS		+=
 MCFLAGS		+=
 LIBDIRS		+=
 LIBS 		+= -lc -lm -lgcc -lrt -lpthread -lasound
+
+SDKDIR		:= $(OUTDIR)/sdk
+
+end:
+	$(Q)echo "[SDK] Generating xstar sdk"
+	$(Q)$(RM) $(SDKDIR)
+	$(Q)$(MKDIR) $(SDKDIR) $(SDKDIR)/inc $(SDKDIR)/lib
+	$(Q)cat $(OUTDIR)/.objects.lst | tr ' ' '\n' | grep '\.o$$' | grep -v 'main\.o' | xargs $(AR) rcs $(SDKDIR)/lib/libxstar.a
+	$(Q)$(CD) $(TOPDIR) && $(FIND) xstar -name '*.h' | xargs $(CP) --parents -t $(SDKDIR)/inc
+	$(Q)$(CD) $(TOPDIR) && $(FIND) packages -name '*.h' | xargs $(CP) --parents -t $(SDKDIR)/inc
+	$(Q)$(CD) $(PRJDIR) && $(FIND) . -name '*.h' -not -path './output/*' | xargs $(CP) --parents -t $(SDKDIR)/inc
+	$(Q)$(CP) $(TOPDIR)/.config.h $(SDKDIR)/inc/
+	$(Q)$(CP) $(TOPDIR)/.config $(SDKDIR)/
+	$(Q)$(CP) $(PRJDIR)/main.c $(SDKDIR)/
+	$(Q)$(CP) $(PKGDIR)/packages.mk $(SDKDIR)/
+	$(Q)$(CP) $(PRJDIR)/Makefile.sdk $(SDKDIR)/Makefile
