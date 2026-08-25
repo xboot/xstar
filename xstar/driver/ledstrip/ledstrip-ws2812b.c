57d5434855bd12ec9768cf81a74c58d5237a4359
Jiang JianJun <8192542@qq.com>
2026-08-25 12:50:44 +0800

    [ws2812b]rename ws2812 to ws2812b and fix timing bug.

--- xstar/driver/ledstrip/ledstrip-ws2812b.c
+++ xstar/driver/ledstrip/ledstrip-ws2812b.c
@@ -26,19 +26,19 @@
 #include <driver/ledstrip/ledstrip.h>
 
 /*
- * WS2812 - WS2812 LED Strip Driver
+ * WS2812B - WS2812B LED Strip Driver
  *
  * Example:
- *   "ledstrip-ws2812": {
+ *   "ledstrip-ws2812b": {
  *       "spi-bus": "spi-f1c100s.0",
  *       "chip-select": 0,
- *       "mode": 0,
+ *       "mode": 3,
  *       "speed": 6400000,
  *       "count": 10
  *   }
  */
 
-struct ledstrip_ws2812_pdata_t {
+struct ledstrip_ws2812b_pdata_t {
 	struct spi_device_t * spidev;
 	int count;
 	struct color_t * color;
@@ -46,9 +46,9 @@ struct ledstrip_ws2812_pdata_t {
 	unsigned char * buffer;
 };
 
-static void ledstrip_ws2812_set_count(struct ledstrip_t * strip, int n)
+static void ledstrip_ws2812b_set_count(struct ledstrip_t * strip, int n)
 {
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
 
 	if((n != pdat->count) && (n > 0))
 	{
@@ -59,58 +59,59 @@ static void ledstrip_ws2812_set_count(struct ledstrip_t * strip, int n)
 		pdat->count = n;
 		pdat->color = xos_mem_malloc(pdat->count * sizeof(struct color_t));
 		xos_memset(pdat->color, 0, pdat->count * sizeof(struct color_t));
-		pdat->buflen = n * 24 + 240;
+		pdat->buflen = 8 + n * 24 + 240;
 		pdat->buffer = xos_mem_malloc(pdat->buflen);
-		xos_memset(&pdat->buffer[0], 0xe0, n * 24);
-		xos_memset(&pdat->buffer[n * 24], 0, 240);
+		xos_memset(&pdat->buffer[0], 0, 8);
+		xos_memset(&pdat->buffer[8], 0xc0, n * 24);
+		xos_memset(&pdat->buffer[8 + n * 24], 0, 240);
 	}
 }
 
-static int ledstrip_ws2812_get_count(struct ledstrip_t * strip)
+static int ledstrip_ws2812b_get_count(struct ledstrip_t * strip)
 {
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
 	return pdat->count;
 }
 
-static void ledstrip_ws2812_set_color(struct ledstrip_t * strip, int i, struct color_t * c)
+static void ledstrip_ws2812b_set_color(struct ledstrip_t * strip, int i, struct color_t * c)
 {
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
-	unsigned char * p = &pdat->buffer[i * 24];
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
+	unsigned char * p = &pdat->buffer[8 + i * 24];
 	int n;
 
 	xos_memcpy(&pdat->color[i], c, sizeof(struct color_t));
 	for(n = 0; n < 8; n++)
-		*p++ = (c->g & (0x80 >> n)) ? 0xf8: 0xe0;
+		*p++ = (c->g & (0x80 >> n)) ? 0xfc: 0xc0;
 	for(n = 0; n < 8; n++)
-		*p++ = (c->r & (0x80 >> n)) ? 0xf8: 0xe0;
+		*p++ = (c->r & (0x80 >> n)) ? 0xfc: 0xc0;
 	for(n = 0; n < 8; n++)
-		*p++ = (c->b & (0x80 >> n)) ? 0xf8: 0xe0;
+		*p++ = (c->b & (0x80 >> n)) ? 0xfc: 0xc0;
 }
 
-static void ledstrip_ws2812_get_color(struct ledstrip_t * strip, int i, struct color_t * c)
+static void ledstrip_ws2812b_get_color(struct ledstrip_t * strip, int i, struct color_t * c)
 {
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
 	xos_memcpy(c, &pdat->color[i], sizeof(struct color_t));
 }
 
-static void ledstrip_ws2812_refresh(struct ledstrip_t * strip)
+static void ledstrip_ws2812b_refresh(struct ledstrip_t * strip)
 {
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
 	spi_device_write_then_read(pdat->spidev, pdat->buffer, pdat->buflen, NULL, 0);
 }
 
-static struct device_t * ledstrip_ws2812_probe(struct driver_t * drv, struct dtnode_t * n)
+static struct device_t * ledstrip_ws2812b_probe(struct driver_t * drv, struct dtnode_t * n)
 {
-	struct ledstrip_ws2812_pdata_t * pdat;
+	struct ledstrip_ws2812b_pdata_t * pdat;
 	struct ledstrip_t * strip;
 	struct device_t * dev;
 	struct spi_device_t * spidev;
 
-	spidev = spi_device_alloc(dt_read_string(n, "spi-bus", NULL), dt_read_int(n, "chip-select", 0), dt_read_int(n, "type", 0), dt_read_int(n, "mode", 0), 8, dt_read_int(n, "speed", 6400000));
+	spidev = spi_device_alloc(dt_read_string(n, "spi-bus", NULL), dt_read_int(n, "chip-select", 0), dt_read_int(n, "type", 0), dt_read_int(n, "mode", 3), 8, dt_read_int(n, "speed", 6400000));
 	if(!spidev)
 		return NULL;
 
-	pdat = xos_mem_malloc(sizeof(struct ledstrip_ws2812_pdata_t));
+	pdat = xos_mem_malloc(sizeof(struct ledstrip_ws2812b_pdata_t));
 	if(!pdat)
 		return NULL;
 
@@ -128,15 +129,15 @@ static struct device_t * ledstrip_ws2812_probe(struct driver_t * drv, struct dtn
 	pdat->buffer = NULL;
 
 	strip->name = alloc_device_name(dt_read_name(n), dt_read_id(n));
-	strip->set_count = ledstrip_ws2812_set_count;
-	strip->get_count = ledstrip_ws2812_get_count;
-	strip->set_color = ledstrip_ws2812_set_color;
-	strip->get_color = ledstrip_ws2812_get_color;
-	strip->refresh = ledstrip_ws2812_refresh;
+	strip->set_count = ledstrip_ws2812b_set_count;
+	strip->get_count = ledstrip_ws2812b_get_count;
+	strip->set_color = ledstrip_ws2812b_set_color;
+	strip->get_color = ledstrip_ws2812b_get_color;
+	strip->refresh = ledstrip_ws2812b_refresh;
 	strip->priv = pdat;
 
-	ledstrip_ws2812_set_count(strip, dt_read_int(n, "count", 1));
-	ledstrip_ws2812_refresh(strip);
+	ledstrip_ws2812b_set_count(strip, dt_read_int(n, "count", 1));
+	ledstrip_ws2812b_refresh(strip);
 
 	if(!(dev = register_ledstrip(strip, drv)))
 	{
@@ -148,10 +149,10 @@ static struct device_t * ledstrip_ws2812_probe(struct driver_t * drv, struct dtn
 	return dev;
 }
 
-static void ledstrip_ws2812_remove(struct device_t * dev)
+static void ledstrip_ws2812b_remove(struct device_t * dev)
 {
 	struct ledstrip_t * strip = (struct ledstrip_t *)dev->priv;
-	struct ledstrip_ws2812_pdata_t * pdat = (struct ledstrip_ws2812_pdata_t *)strip->priv;
+	struct ledstrip_ws2812b_pdata_t * pdat = (struct ledstrip_ws2812b_pdata_t *)strip->priv;
 
 	if(strip)
 	{
@@ -164,31 +165,31 @@ static void ledstrip_ws2812_remove(struct device_t * dev)
 	}
 }
 
-static void ledstrip_ws2812_suspend(struct device_t * dev)
+static void ledstrip_ws2812b_suspend(struct device_t * dev)
 {
 }
 
-static void ledstrip_ws2812_resume(struct device_t * dev)
+static void ledstrip_ws2812b_resume(struct device_t * dev)
 {
 }
 
-static struct driver_t ledstrip_ws2812 = {
-	.name		= "ledstrip-ws2812",
-	.probe		= ledstrip_ws2812_probe,
-	.remove		= ledstrip_ws2812_remove,
-	.suspend	= ledstrip_ws2812_suspend,
-	.resume		= ledstrip_ws2812_resume,
+static struct driver_t ledstrip_ws2812b = {
+	.name		= "ledstrip-ws2812b",
+	.probe		= ledstrip_ws2812b_probe,
+	.remove		= ledstrip_ws2812b_remove,
+	.suspend	= ledstrip_ws2812b_suspend,
+	.resume		= ledstrip_ws2812b_resume,
 };
 
-static void ledstrip_ws2812_driver_init(void)
+static void ledstrip_ws2812b_driver_init(void)
 {
-	register_driver(&ledstrip_ws2812);
+	register_driver(&ledstrip_ws2812b);
 }
 
-static void ledstrip_ws2812_driver_exit(void)
+static void ledstrip_ws2812b_driver_exit(void)
 {
-	unregister_driver(&ledstrip_ws2812);
+	unregister_driver(&ledstrip_ws2812b);
 }
 
-driver_initcall(ledstrip_ws2812_driver_init);
-driver_exitcall(ledstrip_ws2812_driver_exit);
+driver_initcall(ledstrip_ws2812b_driver_init);
+driver_exitcall(ledstrip_ws2812b_driver_exit);
